b99c5b631df8ecaea6a76f398e713a79f234f89f
GeekyTissues <asian.so.called.up.himself@gmail.com>
2024-08-24 13:49:00 +0930

    Game Over Menu
    Restart
    Quit
    Main Menu

---
+++ Assets/Scripts/UI/SelectionArrow.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionArrow : MonoBehaviour
+{
+    [SerializeField] private RectTransform[] options;
+    [SerializeField] private AudioClip changeSound; // Arrow moving
+    [SerializeField] private AudioClip interactSound; // Selections
+
+    private RectTransform rect;
+    private int currentPosition;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        //Change position of the selection arrow
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            ChangePosition(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            ChangePosition(1);
+        }
+
+        //Interact with Options
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Interact();
+        }
+    }
+
+    private void ChangePosition(int _change)
+    {
+        currentPosition += _change;
+
+        if (_change != 0)
+        {
+            SoundManager.instance.PlaySound(changeSound);
+        }
+
+        if (currentPosition < 0)
+        {
+            currentPosition = options.Length - 1;
+        }
+        else if (currentPosition > options.Length - 1)
+        {
+            currentPosition = 0;
+        }
+
+        //Assigns the y position of current option to the arrow
+        rect.position = new Vector3(rect.position.x, options[currentPosition].position.y, 0);
+    }
+
+    private void Interact()
+    {
+        SoundManager.instance.PlaySound(interactSound);
+
+        options[currentPosition].GetComponent<Button>().onClick.Invoke();
+    }
+}
